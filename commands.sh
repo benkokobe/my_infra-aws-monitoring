d492c02160232456c8ab8229353b6283ba347fc3
Bineyam Kokobe <bineyam.kokobe@soprabanking.com>
2023-01-30 22:13:39 +0100

    update command file

--- commands.sh
+++ commands.sh
@@ -1,3 +1,6 @@
+# Infra provisioning for the Monitoring tool on AWS
+
+## Terraform life cycle commands
 terraform fmt -recursive
 terraform init
 
@@ -12,3 +15,9 @@ terraform state list
 
 terraform destroy
 terraform destroy -auto-approve
+
+##  Push to GitHub
+git remote add origin https://github.com/benkokobe/my_infra-aws-monitoring.git
+git branch -M main
+git push -u origin main
+
